fa99533e0e17980981a48493d33859e78b0a9a33
Leung Wong <Leung@Judgement.(none)>
2013-04-21 11:34:29 +0200

    Test commit

--- src/com/groupfun/MainActivity.java
+++ src/com/groupfun/MainActivity.java
@@ -2,14 +2,18 @@ package com.groupfun;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Color;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
-
+//test 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView = (TextView) findViewById(R.id.text);
+        textView.setTextColor(Color.parseColor("red"));
     }
 
 
